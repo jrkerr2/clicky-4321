fc703f9791de13b956c57af91a38e1ae98257e2c
jrkerr2 <johnrkerr@hotmail.com>
2018-10-29 11:35:52 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -14,10 +14,6 @@ class App extends React.Component {
 
   }
 
-  // restartGame = () => {
-  //   forEach(image => id = Math.random())
-  // }
-
   clickHandler = (id, selected) => {
     const imageArray = this.state.images;
     if(!selected) {
@@ -25,15 +21,11 @@ class App extends React.Component {
         if (id === image.id) {
           imageArray[index].selected = true;
         }
-        // console.log(image);
-        // console.log(id,selected); 
-
+        
         const { topScore, score } = this.state
         const newScore = score + 1
         const newTopScore = newScore > topScore ? newScore : topScore;
-        // console.log("Current: " + score, topScore);
-        // console.log("New: " + newScore, newTopScore);
-
+        
         return this.setState({
           score: newScore,
           topScore: newTopScore,
@@ -43,41 +35,22 @@ class App extends React.Component {
 
     }    
     else {
-      // console.log("Sorry! You guessed incorrectly.");
       imageArray.forEach((image,index) => {
         imageArray[index].selected = false;
 
       })
+
       return this.setState({
         message: "Sorry! You guessed incorrectly. Try again.",
         score: 0
       })
-
-     // imageArray.selected = false;
-
+    
     }
     
     return this.setState({
       images: imageArray.sort(() => Math.random() - 0.5)
     });
   };
-  
-  // handleClick = () => {
-
-  //   // check click state of image
-  //   if (image.clicked == true)
-  //       setState(message = "You lose! Try again.")
-  //       restartGame();
-        
-    
-  //   else
-  //   // if clicked = true, then lose
-  //   // if clicked = false, the clicked --> true AND
-  //   // score = score + 1, if score > topScore THEN
-  //   // topScore --> score
-  //   // shuffle cards
-    
-  // }
 
   render() {
     return (
@@ -86,10 +59,8 @@ class App extends React.Component {
           <Jumbo />
 
           <p> { this.state.message } </p>
-          <h3> Score: { this.state.score } | Top Score: { this.state.topScore } </h3>
-          
-          
-          
+          <h3> Score: { this.state.score } | Top Score: { this.state.topScore } </h3>        
+                    
           {this.state.images.map(card => (
             <Cards 
               cardImage={card.image}  
@@ -100,9 +71,7 @@ class App extends React.Component {
               selected={card.selected}            
 
           />))} 
-
-        </div>
-        
+        </div>        
     );
   }
 }
